3479b586e03e48486640f6b4ec9d29c8d254d24c
filippogiovannoni <filippogiovannonii@gmail.com>
2024-06-15 09:38:49 +0200

    add one to many relation between photos and categories

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Category;
 
 class Photo extends Model
 {
@@ -15,5 +16,10 @@ class Photo extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function categories(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
     }
 }
